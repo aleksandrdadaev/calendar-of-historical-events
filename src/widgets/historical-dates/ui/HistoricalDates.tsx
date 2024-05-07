e7b71411110e96e5100d8da9c29bf7b60352d276
Sanchezz <sasha.dadaev.98@mail.ru>
2024-05-07 12:29:03 +0700

    create title component

--- src/widgets/historical-dates/ui/HistoricalDates.tsx
+++ src/widgets/historical-dates/ui/HistoricalDates.tsx
@@ -2,11 +2,13 @@ import { FC } from 'react'
 
 import styles from './HistoricalDates.module.scss'
 import Lines from './lines/Lines'
+import Title from './title/Title'
 
 export const HistoricalDates: FC = () => {
 	return (
 		<section className={styles.wrapper}>
 			<Lines />
+			<Title />
 		</section>
 	)
 }
