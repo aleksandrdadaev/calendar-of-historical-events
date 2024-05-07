d2c9bc601706387e65c7aad9c9c58ff92b1cb5e8
Sanchezz <sasha.dadaev.98@mail.ru>
2024-05-07 10:39:21 +0700

    init again with craco, becuse comand eject is was unsuccessful

--- src/app/router/router.tsx
+++
@@ -1,5 +0,0 @@
-import { createBrowserRouter } from 'react-router-dom'
-
-import { Main } from '@/screens/main'
-
-export const router = createBrowserRouter([{ path: '/', element: <Main /> }])
--- src/screens/main/ui/Main.tsx
+++
@@ -1,5 +0,0 @@
-import { FC } from 'react'
-
-export const Main: FC = () => {
-	return <main>Main page</main>
-}
