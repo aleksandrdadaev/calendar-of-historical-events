947d48643632686b6c513b4109b2801097c26561
Sanchezz <sasha.dadaev.98@mail.ru>
2024-05-07 10:28:17 +0700

    create router

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
 
 import '../src/app/styles/global.scss'
 
+import { router } from './app/router/router'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<main>screen</main>
+		<RouterProvider router={router} />
 	</React.StrictMode>
 )
---
+++ src/screens/main/ui/Main.tsx
@@ -0,0 +1,5 @@
+import { FC } from 'react'
+
+export const Main: FC = () => {
+	return <main>Main page</main>
+}
