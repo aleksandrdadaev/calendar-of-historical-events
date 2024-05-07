f4e0fb860ffa5646027ef42a499e692077daba58
Sanchezz <sasha.dadaev.98@mail.ru>
2024-05-07 11:08:48 +0700

    install sass, add reset styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+
+import '@/app/styles/global.scss'
+
 import { router } from './app/router/router'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
