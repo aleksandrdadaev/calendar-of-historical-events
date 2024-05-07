344afe69e9ca10c0b8df62e1cc9b3e0ad1eaa7fb
Sanchezz <sasha.dadaev.98@mail.ru>
2024-05-07 10:00:44 +0700

    add global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import '../src/app/styles/global.scss'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<main>screen</main>
