9405cc78d31fd61810607d3ca7405ab7e91e348d
Sanchezz <sasha.dadaev.98@mail.ru>
2024-05-08 13:10:48 +0700

    install gsap and add animation numbers

--- src/widgets/historical-dates/ui/dates/Dates.tsx
+++ src/widgets/historical-dates/ui/dates/Dates.tsx
@@ -5,15 +5,16 @@ import { dates } from '@/shared/config/historical-dates.config'
 import { sortDatesByYear } from '../../lib/utils/sort-dates-by-year.util'
 
 import styles from './Dates.module.scss'
+import Number from './number/Number'
 
 const Dates: FC<{ activeIndex: number }> = ({ activeIndex }) => {
 	const sortArray = sortDatesByYear(dates[activeIndex].events)
 	return (
 		<div className={styles.wrapper}>
-			<span>{sortArray[0].year}</span>
+			<Number number={sortArray[0].year} />
 			{'\u00A0'}
 			{'\u00A0'}
-			<span>{sortArray[sortArray.length - 1].year}</span>
+			<Number number={sortArray[sortArray.length - 1].year} />
 		</div>
 	)
 }
