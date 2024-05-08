6af4b53cec1ca1b24e2feeaa94d3d0fc95a9ae64
Sanchezz <sasha.dadaev.98@mail.ru>
2024-05-08 12:36:15 +0700

    create numbers block

--- src/widgets/historical-dates/ui/dates/Dates.tsx
+++ src/widgets/historical-dates/ui/dates/Dates.tsx
@@ -1,4 +1,4 @@
-import { FC, useMemo } from 'react'
+import { FC } from 'react'
 
 import { dates } from '@/shared/config/historical-dates.config'
 
@@ -7,7 +7,15 @@ import { sortDatesByYear } from '../../lib/utils/sort-dates-by-year.util'
 import styles from './Dates.module.scss'
 
 const Dates: FC<{ activeIndex: number }> = ({ activeIndex }) => {
-	return <div className={styles.wrapper}>Dates</div>
+	const sortArray = sortDatesByYear(dates[activeIndex].events)
+	return (
+		<div className={styles.wrapper}>
+			<span>{sortArray[0].year}</span>
+			{'\u00A0'}
+			{'\u00A0'}
+			<span>{sortArray[sortArray.length - 1].year}</span>
+		</div>
+	)
 }
 
 export default Dates
