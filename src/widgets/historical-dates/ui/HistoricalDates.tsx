1ffae48763ead1b2da50d99340108ddfa362a747
Sanchezz <sasha.dadaev.98@mail.ru>
2024-05-09 15:38:04 +0700

    add  lines

--- src/widgets/historical-dates/ui/HistoricalDates.tsx
+++ src/widgets/historical-dates/ui/HistoricalDates.tsx
@@ -3,10 +3,12 @@ import { FC } from 'react'
 import styles from './HistoricalDates.module.scss'
 import MainSlider from './main-slider/MainSlider'
 import Title from './title/Title'
+import VerticalLine from './vertical-line/VerticalLine'
 
 export const HistoricalDates: FC = () => {
 	return (
 		<section className={styles.wrapper}>
+			<VerticalLine />
 			<Title />
 			<MainSlider />
 		</section>
